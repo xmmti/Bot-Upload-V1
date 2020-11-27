545186c875b4b2d00c8ccfc6324d8ec79714820e
X7md <54203033+X7md@users.noreply.github.com>
2020-11-27 18:54:39 +0300

    Add project files.

---
+++ Bot-Upload-V1/Form1.Designer.vb
@@ -0,0 +1,293 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.BackgroundWorker2 = New System.ComponentModel.BackgroundWorker()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button1.Location = New System.Drawing.Point(14, 29)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(105, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Select"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(15, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(105, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Select Profile Picture"
+        '
+        'Button2
+        '
+        Me.Button2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button2.Location = New System.Drawing.Point(156, 29)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(104, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Select"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(152, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(118, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Select Image to Upload"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 47)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(69, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Account List:"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.CheckBox2)
+        Me.GroupBox1.Controls.Add(Me.CheckBox1)
+        Me.GroupBox1.Controls.Add(Me.ListBox1)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Button4)
+        Me.GroupBox1.Controls.Add(Me.Label9)
+        Me.GroupBox1.Controls.Add(Me.Label8)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Location = New System.Drawing.Point(13, 58)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(253, 190)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Login"
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Checked = True
+        Me.CheckBox2.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.CheckBox2.Location = New System.Drawing.Point(9, 146)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(116, 17)
+        Me.CheckBox2.TabIndex = 7
+        Me.CheckBox2.Text = "Upload Post Image"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Checked = True
+        Me.CheckBox1.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.CheckBox1.Location = New System.Drawing.Point(9, 166)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(112, 17)
+        Me.CheckBox1.TabIndex = 7
+        Me.CheckBox1.Text = "Change Profile Pic"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(9, 68)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(238, 56)
+        Me.ListBox1.TabIndex = 3
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(133, 146)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(114, 38)
+        Me.TextBox1.TabIndex = 5
+        '
+        'Button4
+        '
+        Me.Button4.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button4.Location = New System.Drawing.Point(9, 19)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(238, 23)
+        Me.Button4.TabIndex = 2
+        Me.Button4.Text = "List User:Pass"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(6, 128)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(48, 13)
+        Me.Label9.TabIndex = 4
+        Me.Label9.Text = "Options:"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(132, 128)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(72, 13)
+        Me.Label8.TabIndex = 4
+        Me.Label8.Text = "Caption Post:"
+        '
+        'Button5
+        '
+        Me.Button5.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button5.Location = New System.Drawing.Point(191, 294)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 6
+        Me.Button5.Text = "Start"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(19, 252)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(117, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Done Upload picture: 0"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.ForeColor = System.Drawing.Color.DarkRed
+        Me.Label5.Location = New System.Drawing.Point(19, 269)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(116, 13)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "Error Upload picture: 0"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(19, 286)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(137, 13)
+        Me.Label6.TabIndex = 7
+        Me.Label6.Text = "Done Change Profile_pic: 0"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.ForeColor = System.Drawing.Color.DarkRed
+        Me.Label7.Location = New System.Drawing.Point(19, 303)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(136, 13)
+        Me.Label7.TabIndex = 7
+        Me.Label7.Text = "Error Change Profile_pic: 0"
+        '
+        'BackgroundWorker1
+        '
+        '
+        'BackgroundWorker2
+        '
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(191, 264)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 8
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(279, 326)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Priv8 Bot -mmti"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents BackgroundWorker2 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Button3 As Button
+End Class
---
+++ Bot-Upload-V1/Form1.vb
@@ -0,0 +1,322 @@
+﻿Imports System.Text.RegularExpressions
+Imports System.String
+Imports System.Text
+Imports System.Web
+Imports System.Net
+Public Class Form1
+    Dim Profilepicture, imagepost As Byte()
+    Dim profilepic_mime_type, imagepost_mime_type As String
+    Dim cookielist As New List(Of String)
+    Dim doneprofile, doneimage, errorprofile, errorimage As Integer
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        BackgroundWorker2.RunWorkerAsync()
+    End Sub
+
+#Region "Change Profile pictrue"
+    Function change_profile_pic(pic As Byte(), cookies_ As String, mime_type As String)
+        Dim tok As String = RandomString(16)
+        Using w As New Net.WebClient
+            w.Headers.Add("Accept: */*")
+            w.Headers.Add("Content-Type: multipart/form-data; boundary=----WebKitFormBoundary" + tok)
+            w.Headers.Add("Cookie", cookies_)
+            w.Headers.Add("User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36")
+            w.Headers.Add("X-CSRFToken", Regex.Match(cookies_, "csrftoken=(.*?);").Groups(1).Value)
+            w.Headers.Add("X-IG-App-ID: 936619743392459")
+            w.Headers.Add("X-Instagram-AJAX: 1")
+            w.Headers.Add("X-Requested-With: XMLHttpRequest")
+            Dim data_post As New StringBuilder
+            data_post.AppendLine("------WebKitFormBoundary" + tok)
+            data_post.AppendLine("Content-Disposition: form-data; name=""profile_pic""; filename=""profilepic.jpg""")
+            data_post.AppendLine("Content-Type: " + mime_type)
+            data_post.AppendLine(String.Empty)
+            data_post.AppendLine(System.Text.Encoding.Default.GetString(pic).ToString)
+            data_post.AppendLine(String.Format("------WebKitFormBoundary{0}--", tok))
+            Try
+                Dim rep As String = w.UploadString("https://i.instagram.com/accounts/web_change_profile_picture/", "POST", data_post.ToString)
+                Dim Stringrsp As String = rep
+                If Stringrsp.Contains("""changed_profile"": true") Then
+                    Return True
+                End If
+            Catch ex As Net.WebException
+                MsgBox(ex.Message)
+                MsgBox(New IO.StreamReader(ex.Response.GetResponseStream).ReadToEnd)
+            End Try
+
+        End Using
+        Return False
+    End Function
+#End Region
+    Function RandomString(ByRef Length As String) As String
+        Dim str As String = Nothing
+        Dim rnd As New Random
+        For i As Integer = 0 To Length
+            Dim chrInt As Integer = 0
+            Do
+                chrInt = rnd.Next(30, 122)
+                If (chrInt >= 48 And chrInt <= 57) Or (chrInt >= 65 And chrInt <= 90) Or (chrInt >= 97 And chrInt <= 122) Then
+                    Exit Do
+                End If
+            Loop
+            str &= Chr(chrInt)
+        Next
+        Return str
+    End Function
+    Dim listacc() As String
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim op As New OpenFileDialog() With {.Filter = "Text File|*.txt", .Multiselect = False, .Title = "List User:Pass", .InitialDirectory = Application.StartupPath}
+        If op.ShowDialog = DialogResult.OK Then
+            listacc = IO.File.ReadAllLines(op.FileName)
+            DirectCast(sender, Button).ForeColor = Color.Green
+            BackgroundWorker1.RunWorkerAsync()
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim op As New OpenFileDialog() With {.Filter = "Image|*.jpg; *.png; *.jfif", .Multiselect = False, .Title = "ProfilePicture Image"}
+        If op.ShowDialog = DialogResult.OK Then
+            If MimeMapping.GetMimeMapping(op.FileName).Contains("image") Then
+                Profilepicture = IO.File.ReadAllBytes(op.FileName)
+                profilepic_mime_type = MimeMapping.GetMimeMapping(op.FileName)
+                DirectCast(sender, Button).ForeColor = Color.Green
+            End If
+
+        End If
+    End Sub
+    Dim Widthimg, Heightimg As Integer
+
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        For Each acc As String In listacc
+            Dim combo_() As String = acc.Split(":")
+            If login(combo_(0), combo_(1)) Then
+                ListBox1.Invoke(Sub() ListBox1.Items.Add(combo_(0)))
+            End If
+
+        Next
+        MsgBox("Done Add Accounts")
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        MsgBox(DirectCast(sender, Button).Text)
+    End Sub
+
+
+    Private Sub BackgroundWorker2_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker2.DoWork
+        If cookielist.Count = Nothing Then
+            MsgBox("Add Account")
+            Exit Sub
+        End If
+        Button5.Invoke(Sub() Button5.Enabled = False)
+        For Each cookie As String In cookielist
+            If CheckBox1.Checked Then
+                If change_profile_pic(Profilepicture, cookie, profilepic_mime_type) Then
+                    doneprofile += 1
+                Else
+                    errorprofile += 1
+                End If
+            End If
+            If CheckBox2.Checked Then
+                Try
+                    Dim micro = (DateTime.Now - New DateTime(1970, 1, 1, 0, 0, 0, 0)).TotalMilliseconds.ToString.Split(".")
+                    '  MsgBox(micro(0))
+                    If uploud_img(cookie, micro(0), imagepost, Heightimg, Widthimg) Then
+                        doneimage += 1
+                    Else
+                        errorimage += 1
+                    End If
+                Catch ex As Exception
+                    MsgBox("Error", MsgBoxStyle.Critical, "")
+                End Try
+            End If
+            Label4.Invoke(Sub() Label4.Text = String.Format("Done Upload picture: {0}", doneimage))
+            Label5.Invoke(Sub() Label5.Text = String.Format("Error Upload picture: {0}", errorimage))
+            Label6.Invoke(Sub() Label6.Text = String.Format("Done Change Profile_pic: {0}", doneprofile))
+            Label7.Invoke(Sub() Label7.Text = String.Format("Error Change Profile_pic: {0}", errorprofile))
+        Next
+        Button5.Invoke(Sub() Button5.Enabled = True)
+        MsgBox("Done!")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim op As New OpenFileDialog() With {.Filter = "Image|*.jpg; *.png; *.jfif", .Multiselect = False, .Title = "Post Image"}
+        If op.ShowDialog = DialogResult.OK Then
+            Dim bmp As New Bitmap(op.FileName)
+            If Not MimeMapping.GetMimeMapping(op.FileName).Contains("jpg") Then
+                Dim ms = New IO.MemoryStream()
+                bmp.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg)
+                imagepost = ms.ToArray()
+                Widthimg = bmp.Width
+                Heightimg = bmp.Height
+            Else
+                Widthimg = bmp.Width
+                Heightimg = bmp.Height
+                imagepost = IO.File.ReadAllBytes(op.FileName)
+                imagepost_mime_type = MimeMapping.GetMimeMapping(op.FileName)
+                DirectCast(sender, Button).ForeColor = Color.Green
+            End If
+            DirectCast(sender, Button).ForeColor = Color.Green
+        End If
+
+    End Sub
+#Region "Login"
+    Function login(Username, Password) As Boolean
+        ' By ...RXLib.dll
+        Try
+            Dim csrftoken As String = get_token()
+            Dim postData As String = "username=" & Username & "&enc_password=" & "#PWD_INSTAGRAM_BROWSER:0:" & (DateTime.Now - New DateTime(1970, 1, 1, 0, 0, 0, 0)).TotalMilliseconds.ToString.Split()(0) & ":" & Password
+            Dim tempcook As New CookieContainer
+            Dim en As New UTF8Encoding
+            Dim byteData As Byte() = en.GetBytes(postData)
+
+            Dim httpPost = DirectCast(WebRequest.Create("https://www.instagram.com/accounts/login/ajax/"), HttpWebRequest)
+
+            httpPost.Method = "POST"
+            httpPost.KeepAlive = True
+            httpPost.ContentType = "application/x-www-form-urlencoded"
+            httpPost.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36"
+            httpPost.ContentLength = byteData.Length
+            httpPost.Headers.Add("x-csrftoken", csrftoken)
+            httpPost.Headers.Add("X-Instagram-AJAX", "1")
+            httpPost.Headers.Add("x-requested-with", "XMLHttpRequest")
+
+            'Send Data
+            Dim poststr As IO.Stream = httpPost.GetRequestStream()
+            poststr.Write(byteData, 0, byteData.Length)
+            poststr.Close()
+
+            'Get Response
+            Dim POST_Response As HttpWebResponse
+            POST_Response = DirectCast(httpPost.GetResponse(), HttpWebResponse)
+
+            Dim tt As String
+            tt = POST_Response.Headers("Set-Cookie")
+
+            Dim cookie = Regex.Match(tt, "ig_did=(.*?;|$)").Value & " " & Regex.Match(tt, "csrftoken=.*?[;|$]").Value & " " & Regex.Match(tt, "mid=.*?[;|$]").Value & " " & Regex.Match(tt, "ds_user_id=.*?[;|$]").Value & " " & Regex.Match(tt, "sessionid=.*?[;|$]").Value
+            Dim Post_Reader As New IO.StreamReader(POST_Response.GetResponseStream())
+            Dim Response As String = Post_Reader.ReadToEnd
+            If Response.Contains("authenticated"": true") Then
+                cookielist.Add(cookie)
+                Return True
+            Else
+                Return False
+            End If
+        Catch ex As Exception
+            Return False
+        End Try
+    End Function
+    Function get_token() As String
+
+        Try
+            Dim httpGet = DirectCast(WebRequest.Create("https://www.instagram.com/"), HttpWebRequest)
+            httpGet.Method = "GET"
+            'httpGet.ContentType = ""
+            httpGet.Headers.Add("X-Instagram-AJAX", "1")
+            httpGet.Headers.Add("x-requested-with", "XMLHttpRequest")
+
+            Dim Get_Response As HttpWebResponse
+            Get_Response = DirectCast(httpGet.GetResponse(), HttpWebResponse)
+
+            Dim Get_Reader As New IO.StreamReader(Get_Response.GetResponseStream())
+
+            Return Regex.Match(Get_Reader.ReadToEnd, """csrf_token"":""(\w+)""").Groups(1).Value
+
+        Catch ex As Exception
+            Return "missing"
+        End Try
+    End Function
+#End Region
+#Region "Upload Image"
+    Function uploud_img(acc_cookies As String, time_nowis As String, imgbyts As Byte(), imgHG As String, imgwd As String) As Boolean
+        Dim Rcs As New Regex("csrftoken=(\w+)(;|$)")
+        Dim csrftoken = Rcs.Match(acc_cookies).Groups(1).Value
+        Try
+            Dim en As New UTF8Encoding
+            ' Dim postData As String = Encoding.Default.GetString(imgby)
+            Dim tempcook As New CookieContainer
+
+            Dim byteData As Byte() = imgbyts
+
+            Dim httpPost = DirectCast(WebRequest.Create($"https://www.instagram.com/rupload_igphoto/fb_uploader_{time_nowis}"), HttpWebRequest)
+
+            httpPost.Host = "i.instagram.com"
+            httpPost.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36"
+            httpPost.Accept = "*/*"
+            httpPost.Method = "POST"
+            httpPost.KeepAlive = True
+            httpPost.ContentType = "image/jpeg"
+            httpPost.ContentLength = byteData.Length
+            httpPost.Headers.Add("Cookie", acc_cookies)
+            httpPost.Headers.Add("X-Instagram-Rupload-Params: {""media_type"":1,""upload_id"":""" & time_nowis & """,""upload_media_height"":" & imgHG & ",""upload_media_width"":" & imgwd & "}")
+            httpPost.Headers.Add("X-Entity-Name: fb_uploader_" & time_nowis)
+            httpPost.Headers.Add("X-Entity-Length", byteData.Length.ToString)
+            httpPost.Headers.Add("x-csrftoken", csrftoken)
+            httpPost.Headers.Add("X-Instagram-AJAX", "1")
+            httpPost.Headers.Add("x-requested-with", "XMLHttpRequest")
+            httpPost.Headers.Add("Offset: 0")
+            'Send Data
+            Dim poststr As IO.Stream = httpPost.GetRequestStream()
+            poststr.Write(byteData, 0, byteData.Length)
+            poststr.Close()
+
+            'Get Response
+            Dim POST_Response As HttpWebResponse
+            POST_Response = DirectCast(httpPost.GetResponse(), HttpWebResponse)
+
+            Dim Post_Reader As New IO.StreamReader(POST_Response.GetResponseStream())
+            Dim Response As String = Post_Reader.ReadToEnd
+            Debug.WriteLine(Response)
+            If share_it(acc_cookies, time_nowis) Then
+                Return True
+            End If
+        Catch ex As WebException : End Try
+        Return False
+    End Function
+    Function share_it(acc_cookies, id_time) As Boolean
+        Dim Rcs As New Regex("csrftoken=(\w+)(;|$)")
+        Dim csrftoken = Rcs.Match(acc_cookies).Groups(1).Value
+        Try
+            Dim en As New UTF8Encoding
+            Dim postData As String = "upload_id=" & id_time & "&caption=" & TextBox1.Text.Replace(vbCrLf, "%0D%0A").Replace(" ", "+") & "&usertags=&custom_accessibility_caption=&retry_timeout="
+            Dim tempcook As New CookieContainer
+
+            Dim byteData As Byte() = en.GetBytes(postData)
+
+
+            Dim httpPost = DirectCast(WebRequest.Create($"https://www.instagram.com/create/configure/"), HttpWebRequest)
+
+            httpPost.Host = "i.instagram.com"
+            httpPost.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36"
+            httpPost.Accept = "*/*"
+            httpPost.Method = "POST"
+            httpPost.KeepAlive = True
+            httpPost.ContentType = "application/x-www-form-urlencoded"
+            httpPost.ContentLength = byteData.Length
+            httpPost.Headers.Add("Cookie", acc_cookies)
+            httpPost.Headers.Add("x-csrftoken", csrftoken)
+            httpPost.Headers.Add("X-Instagram-AJAX", "1")
+            httpPost.Headers.Add("x-requested-with", "XMLHttpRequest")
+            'Send Data
+            Dim poststr As IO.Stream = httpPost.GetRequestStream()
+            poststr.Write(byteData, 0, byteData.Length)
+            poststr.Close()
+
+            'Get Response
+            Dim POST_Response As HttpWebResponse
+            POST_Response = DirectCast(httpPost.GetResponse(), HttpWebResponse)
+
+            Dim Post_Reader As New IO.StreamReader(POST_Response.GetResponseStream())
+            Dim Response As String = Post_Reader.ReadToEnd
+            If Response.Contains("""status"": ""ok") Then
+                Return True
+            Else
+                Return False
+            End If
+        Catch ex As WebException
+            Dim rsponString As String = New IO.StreamReader(ex.Response.GetResponseStream).ReadToEnd
+            If rsponString IsNot Nothing Then
+                MsgBox(rsponString, MsgBoxStyle.Critical)
+            End If
+            Return False
+        End Try
+    End Function
+#End Region
+End Class
